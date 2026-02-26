40e237fa68432c654e494793a1e268b1b2941300
Stevan Pabon <spabonc@unimayor.edu.co>
2026-02-26 17:56:17 -0500

    creacion de componenetes

---
+++ src/app/product-cart/product-car.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-car',
+  imports: [],
+  templateUrl: './product-car.html',
+  styleUrl: './product-car.css',
+})
+export class ProductCar {
+
+}

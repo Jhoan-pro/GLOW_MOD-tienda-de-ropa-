ab86cda88e74a19a69b01796023344bc5e800c61
Stevan Pabon <spabonc@unimayor.edu.co>
2026-03-04 13:42:55 -0500

    conexión de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { RegisterUser } from './register-user/register-user';
+import { ForgotPassword } from './forgot-password/forgot-password';
+import { Admin } from './admin/admin';
+import { Cashier } from './cashier/cashier';
+import { Client } from './client/client';
+import { Cart } from './cart/cart';
+import { Invoice } from './invoice/invoice';
+import { ProductForm } from './product-form/product-form';
+
+export const routes: Routes = [
+
+  { path: '', component: Home },
+
+  { path: 'login', component: Login },
+  { path: 'register', component: RegisterUser },
+  { path: 'forgot-password', component: ForgotPassword },
+
+  { path: 'admin', component: Admin },
+  { path: 'cashier', component: Cashier },
+  { path: 'client', component: Client },
+
+  { path: 'cart', component: Cart},
+  { path: 'invoice', component: Invoice },
+
+  { path: 'product/new', component: ProductForm },
+
+  { path: '**', redirectTo: '' }
+];

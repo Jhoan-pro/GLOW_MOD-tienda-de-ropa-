8d83a9e4becf6150e6b80f8dad6cc2c43f4514c7
Jhoan1316 <jhoandiegom@gmail.com>
2026-03-19 12:13:12 -0500

    validacion1

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
+  email: string = '';
+  password: string = '';
+  errorMsg: string = '';
+
+  login() {
+
+    // Validación: campos vacíos
+    if (!this.email || !this.password) {
+      this.errorMsg = 'Todos los campos son obligatorios';
+      return;
+    }
+
+    // Validación: formato de correo
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(this.email)) {
+      this.errorMsg = 'Correo electrónico inválido';
+      return;
+    }
+
+    // Validación: longitud de contraseña
+    if (this.password.length < 6) {
+      this.errorMsg = 'La contraseña debe tener mínimo 6 caracteres';
+      return;
+    }
+
+    // Simulación de login
+    if (this.email === 'admin@gmail.com' && this.password === '123456') {
+      this.errorMsg = '';
+      alert('Login exitoso');
+    } else {
+      this.errorMsg = 'Credenciales incorrectas';
+    }
+  }
 }

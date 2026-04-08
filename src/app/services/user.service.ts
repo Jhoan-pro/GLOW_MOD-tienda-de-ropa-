d905f4784dfec7332b6028c9ac20d9b97d72ffea
Jhoan1316 <jhoandiegom@gmail.com>
2026-04-07 23:28:59 -0500

    validacion de localStorage en node

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,21 +1,31 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { platformBrowser } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private storageKey = 'app_users';
+  private platformid = inject(PLATFORM_ID);
 
   getUsers(): User[] {
-    const users = localStorage.getItem(this.storageKey);
-    return users ? JSON.parse(users) : [];
+    if (isPlatformBrowser(this.platformid)) {
+      const users = localStorage.getItem(this.storageKey);
+      return users ? JSON.parse(users) : [];
+    }
+    return [];
   }
 
   addUser(user: User) {
-    const users = this.getUsers();
-    user.id = Date.now(); // ID simple
-    users.push(user);
-    localStorage.setItem(this.storageKey, JSON.stringify(users));
+    if (isPlatformBrowser(this.platformid)) {
+      const users = this.getUsers();
+      user.id = Date.now(); // ID simple
+      users.push(user);
+      localStorage.setItem(this.storageKey, JSON.stringify(users));
+    }
+    return [];
   }
 }

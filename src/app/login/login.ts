653b985e11ebeb4ba8e61e8f05774775f7e309e6
Jhoan1316 <jhoandiegom@gmail.com>
2026-04-07 23:22:05 -0500

    validacion de usuario existente en login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,16 @@ import { Router, RouterLink } from '@angular/router';
   styleUrl: './login.css',
 })
 export class Login {
-
   email: string = '';
   password: string = '';
   message: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private UserService: UserService,
+  ) {}
 
   login() {
-
     // Validación: campos vacíos
     if (!this.email || !this.password) {
       this.message = 'Todos los campos son obligatorios';
@@ -41,25 +43,39 @@ export class Login {
 
     // Simulación de login
     if (this.email === 'admin@gmail.com' && this.password === '123456') {
-
       this.message = '';
 
       // REDIRECCIÓN
       this.router.navigate(['/admin-dashboard/admin']);
-
     } else {
       this.message = 'Credenciales incorrectas';
     }
-    
 
-    if (this.email === 'cli@gmail.com' && this.password === '1234567'){
+    if (this.email === 'cli@gmail.com' && this.password === '1234567') {
       this.message = 'Error';
 
       this.router.navigate(['./home']);
+    }
+    
+   // validacion de usuarios 
+    const users = this.UserService.getUsers();
 
+    const userFound = users.find((u) => u.email === this.email && u.password === this.password);
+
+    if (userFound) {
+      this.message = '';
+
+      if (userFound.role === 'admin') {
+        this.router.navigate(['/admin-dashboard/admin']);
+      } else if (userFound.role === 'cashier') {
+        this.router.navigate(['/dashboard/cashier']);
+      } else {
+     
+        this.router.navigate(['/']);
+      }
+    } else {
       
+      this.message = 'Correo o contraseña incorrectos';
     }
   }
-
-  
 }

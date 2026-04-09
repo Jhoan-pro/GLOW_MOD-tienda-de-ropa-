8c40218413b9d2b09ca6bc4b73b5006628235732
Jhoan1316 <jhoandiegom@gmail.com>
2026-04-09 12:04:03 -0500

    solucion de errores login

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -14,7 +14,7 @@ import { CommonModule } from '@angular/common';
 export class Login {
   email: string = '';
   password: string = '';
-  
+
   errors: any = {
     email: '',
     password: '',
@@ -27,12 +27,11 @@ export class Login {
   ) {}
 
   login() {
-
-  // Validación: campos vacíos
-  if (!this.email || !this.password) {
-    this.message = 'Todos los campos son obligatorios';
-    return;
-  }
+    // Validación: campos vacíos
+    if (!this.email || !this.password) {
+      this.errors.general = 'Todos los campos son obligatorios';
+      return;
+    }
 
     this.errors = { email: '', password: '', general: '' };
     // Validación: campos vacíos
@@ -44,7 +43,6 @@ export class Login {
     }
 
     if (this.errors.email || this.errors.password) return;
-
 
     // Validación: formato de correo
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -70,65 +68,58 @@ export class Login {
       this.errors.general = 'Credenciales incorrectas';
     }
 
-
-
-   // validacion de usuarios
+    // validacion de usuarios
     const users = this.UserService.getUsers();
 
     const userFound = users.find((u) => u.email === this.email && u.password === this.password);
 
     if (userFound) {
-      this.message = '';
+      this.errors.general = '';
 
-
-    this.UserService.login(userFound);
+      this.UserService.login(userFound);
       if (userFound.role === 'admin') {
         this.router.navigate(['/admin-dashboard/admin']);
       } else if (userFound.role === 'cashier') {
         this.router.navigate(['/dashboard/cashier']);
       } else {
-
         this.router.navigate(['/']);
       }
     } else {
+      this.errors.general = 'Correo o contraseña incorrectos';
 
-      this.message = 'Correo o contraseña incorrectos';
+      if (this.email === 'cli@gmail.com' && this.password === '123456') {
+        this.errors = '';
 
-    if (this.email === 'cli@gmail.com' && this.password === '123456') {
-      this.errors = '';
-
-      // REDIRECCIÓN
-      this.router.navigate(['/home']);
-    } else {
-      this.errors.general = 'Credenciales incorrectas';
-    }
-
-    // validacion de usuarios
-const users = this.UserService.getUsers();
-    const userFound = users.find(u => u.email === this.email && u.password === this.password);
-
-    if (userFound) {
-      if (!userFound.active) {
-        this.errors.general = 'Tu cuenta ha sido deshabilitada. Contacta al admin.';
-        return;
+        // REDIRECCIÓN
+        this.router.navigate(['/home']);
+      } else {
+        this.errors.general = 'Credenciales incorrectas';
       }
-      
-      // Redirección según rol
-      this.redirectByRole(userFound.role);
-    } else {
-      this.errors.general = 'Credenciales incorrectas';
 
+      // validacion de usuarios
+      const users = this.UserService.getUsers();
+      const userFound = users.find((u) => u.email === this.email && u.password === this.password);
+
+      if (userFound) {
+        if (!userFound.active) {
+          this.errors.general = 'Tu cuenta ha sido deshabilitada. Contacta al admin.';
+          return;
+        }
+
+        // Redirección según rol
+        this.redirectByRole(userFound.role);
+      } else {
+        this.errors.general = 'Credenciales incorrectas';
+      }
     }
   }
-
   private redirectByRole(role: string) {
     if (role === 'admin') this.router.navigate(['/admin-dashboard/admin']);
     else if (role === 'cashier') this.router.navigate(['/dashboard/cashier']);
     else this.router.navigate(['/']);
   }
-  
-  volver(){
-    this.router.navigate(['/register'])
+
+  volver() {
+    this.router.navigate(['/register']);
   }
-  
 }

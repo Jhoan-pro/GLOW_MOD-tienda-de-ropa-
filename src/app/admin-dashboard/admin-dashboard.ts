ce2b73f8348fc1d4f2e9e321c1e5de186b7d9adb
Jhoan1316 <jhoandiegom@gmail.com>
2026-04-29 11:25:14 -0500

    mejora de estilo de cerrar sesion en admin

--- src/app/admin-dashboard/admin-dashboard.ts
+++ src/app/admin-dashboard/admin-dashboard.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
 export class AdminDashboard {
   sidebarOpen = false;
   constructor(private router: Router) {}
+  mostrarConfirmLogout = false;
+  
 
   logout() {
-    const confirmacion = confirm('¿Seguro que deseas cerrar sesión?');
+  this.mostrarConfirmLogout = true;
+}
 
-    if (confirmacion) {
-      this.router.navigate(['/register']);
-    }
-  }
+confirmarLogout() {
+  this.router.navigate(['/login']);
+}
 }

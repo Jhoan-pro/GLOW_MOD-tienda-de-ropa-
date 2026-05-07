fa5318572dbfb1c17aca25b2f36ca1edb9eb5cca
jhoan-pro <jdmunozm@unimayor.edu.co>
2026-05-07 15:55:59 -0500

    correccion de redireccion de sub admin

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -76,7 +76,7 @@ export class Login {
   }
   private redirectByRole(role: string) {
   if (role === 'admin') this.router.navigate(['/admin-dashboard/admin']);
-  else if (role === 'sub-admin') this.router.navigate(['/admin-dashboard/admin']);
+  else if (role === 'sub-admin') this.router.navigate(['/admin-dashboard/dashBoard']);
   else if (role === 'cashier') this.router.navigate(['/dashboard/cashier']);
   else this.router.navigate(['/']);
   }
